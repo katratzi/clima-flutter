44b3b9ef4085e035708cabf21adad695e140c63a
katratzi <ric_clifford@hotmail.com>
2020-04-30 21:28:12 +0100

    refactor location call into own class as an asynchronous call.

---
+++ lib/services/location.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double longitude;
+  double latitude;
+
+  Location({this.longitude, this.latitude});
+
+  Future<void> getCurrentLocation() async {
+    Position position;
+    try {
+      position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    } catch (e) {
+      print(e);
+    }
+
+    // fallback - buckingham palace
+    this.longitude = position.longitude ?? 51.5114;
+    this.latitude = position.latitude ?? 0.1419;
+  }
+}
