35b116459a27d194bd277c6f8b1891c38e1fa8d8
katratzi <ric_clifford@hotmail.com>
2020-05-01 18:37:00 +0100

    importing http package to get data from openweathermap

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -25,8 +26,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('longitude: ${location.longitude} latitude:${location.latitude}');
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+    // print(response.statusCode);
+    // print(response.body);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
