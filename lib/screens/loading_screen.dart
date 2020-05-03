6e6b2aaf5973166c8e72cb81262458c4620d6123
katratzi <ric_clifford@hotmail.com>
2020-05-03 22:22:15 +0100

    adding a little load animation with flutter_spinkit package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter/material.dart';
+
+import 'location_screen.dart';
 
 const apiKey = 'e72ca729af228beabd5d20e3b7749713';
 
@@ -35,16 +40,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lon=$longitude&lat=$latitude&appid=$apiKey');
 
+    // simulate a small delay
+    sleep(Duration(seconds: 3));
     var weatherData = await networkHelper.getData();
 
-    double temperature = weatherData['main']['temp'];
-    int condition = weatherData['weather'][0]['id'];
-    String city = weatherData['name'];
-    print('temperature.$temperature condition.$condition city.$city');
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return LocationScreen();
+      }),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: const SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
