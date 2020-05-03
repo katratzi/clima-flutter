a67ed71bc7dbafa05481217a9af9892cf602933d
katratzi <ric_clifford@hotmail.com>
2020-05-03 21:36:43 +0100

    refactor networking into it's own class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 const apiKey = 'e72ca729af228beabd5d20e3b7749713';
 
@@ -18,7 +16,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
   @override
@@ -27,35 +25,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('deactivate called');
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print('longitude: ${location.longitude} latitude:${location.latitude}');
     this.longitude = location.longitude;
     this.latitude = location.latitude;
-    getData();
-  }
+    print('longitude: ${location.longitude} latitude:${location.latitude}');
 
-  void getData() async {
-    http.Response response = await http.get(
+    NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lon=$longitude&lat=$latitude&appid=$apiKey');
-    // print(response.statusCode);
-    // print(response.body);
-    if (response.statusCode == 200) {
-      String data = response.body;
-      print(data);
 
-      var decodedData = jsonDecode(data);
-      // temperature
-      double temperature = decodedData['main']['temp'];
-      // condition
-      int condition = decodedData['weather'][0]['id'];
-      // city name
-      String city = decodedData['name'];
-      print('temperature.$temperature condition.$condition city.$city');
-    } else {
-      print(response.statusCode);
-    }
+    var weatherData = await networkHelper.getData();
+
+    double temperature = weatherData['main']['temp'];
+    int condition = weatherData['weather'][0]['id'];
+    String city = weatherData['name'];
+    print('temperature.$temperature condition.$condition city.$city');
   }
 
   @override
